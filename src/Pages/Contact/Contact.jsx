d9c4816eec68d2810e0f714d6f3ffc478923fde8
Artem <protastema1803@gmail.com>
2023-09-13 23:26:49 +0300

    Fixed emailContact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import { Container } from 'Pages/About/About.styled';
 import EmailContact from 'components/ContactComponents/EmailContact/EmailContact';
 import SocialContact from 'components/ContactComponents/SocialContact/SocialContact';
 
+
 export const Contact = () => {
   return (
     <Container>
